3c1464c8d77145f5efa394d10fa57970d13e0a9d
root <root@server.gohost.kz>
2023-08-26 20:38:06 +0000

    edited settings.py

--- auth_app/settings.py
+++ auth_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '109.248.170.181', 'http://localhost:5173']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '109.248.170.181', 'http://localhost:5173', 'https://qosyl.me/']
 
 
 # Application definition
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'django_extensions',
+    'sslserver',
     'django.contrib.postgres',
     'main.apps.MainConfig'
 ]
